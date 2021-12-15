3f12ab3c12a6a0d97a3efc5a81e422c626a1edfa
BenjaminEchelmeier <benjaminechlmeier@gmail.com>
2021-12-15 11:59:13 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# Uses setuptools to package this project. Almost all variables are parsed from pyproject.toml.
+# Also can download codepacks (such as framework) into the project.
+
 from pathlib import Path
 from setuptools import setup
 from setuptools import find_packages
@@ -9,79 +12,50 @@ from urllib import request
 from zipfile import ZipFile
 
 
-def normalize(name):
+def normalize(name: str) -> str:
+    """Simple function to normalize and pythonize names"""
     return re.sub(r"[-_.]+", "-", name).lower()
 
 
-def _parse_pyproject():
-    pyproject_path = "pyproject.toml"
+def _parse_pyproject(pyproject_path: str) -> str:
+    """Opens a pyproject.toml at 'pyproject_path' and returns the contents as a dictionary."""
     pp = Path(pyproject_path)
     return toml.load(pp.open())
 
 
-def create_dirs(dirs):
+def create_dirs(dirs: list[str]) -> None:
+    """Creates the directories in dirs"""
     for dir in dirs:
         if not path.exists(dir):
             mkdir(dir)
 
 
-def dl_bash_repos(repos, _tmp):
-    for repo in repos:
-        request.urlretrieve(repo["url"], repo["filename"])
-        with ZipFile(repo["filename"], "r") as zip_ref:
-            zip_ref.extractall(_tmp)
-
-
-# VARS
-pyproject = _parse_pyproject()
-project_name = normalize(pyproject["project"]["name"])
-src = pyproject["tool"]["bem"]["source-directory"]
-dirs = [".tmp", ".tmp/download", ".tmp/logs"]
-# BEM setup and framework import
-
-create_dirs(dirs)
-
-# repos = [
-#     {
-#         "url": "https://github.com/terminal-labs/bash-environment-shelf/archive/refs/heads/master.zip",
-#         "filename": ".tmp/download/bash-environment-shelf.zip",
-#     }
-# ]
-shelf_name = ".tmp/download/bash-environment-shelf.zip"
-
-
-def install_codepacks(bem_shelf):
-    print(bem_shelf)
+def install_codepacks(bem_shelf: dict[str, str]) -> None:
+    """Consumes a dictionary of codepack names and the url to gather them from.
+    Downloads from the url and extracts the codepack to the top level of the project.
+    Note that this is in development and currently only works with bem-shelf-master."""
     for codepack, url in bem_shelf.items():
         request.urlretrieve(url, shelf_name)
         with ZipFile(shelf_name, "r") as zip_ref:
             zip_ref.extractall(".tmp")
         if src:
-            cpack_path = Path(src + "/" + project_name + "/" + "framework")
+            cpack_path = Path(src + "/" + project_name + "/" + codepack)
         else:
-            cpack_path = Path(project_name + "/" + "framework")
+            cpack_path = Path(project_name + "/" + codepack)
         shutil.rmtree(cpack_path, ignore_errors=True)
         shutil.copytree(".tmp/bash-environment-shelf-master/codepacks/" + codepack, cpack_path)
 
 
-install_codepacks(pyproject.get("tool", {}).get("bem", {}).get("codepack", {}))
-# dl_bash_repos(repos, ".tmp")
-
-# _path_to_framework = src + "/" + project_name + "/" + "framework"
-# if path.exists(_path_to_framework):
-#     shutil.rmtree(_path_to_framework)
-
-# if not path.exists(_path_to_framework):
-#     shutil.copytree(".tmp/bash-environment-shelf-master/codepacks/framework", _path_to_framework)
-
-
-# setuptools setup
-def parse_extras_require(pyproject):
+def parse_extras_require(pyproject: dict[str, dict[str, list[str]]]) -> dict[str, list[str]]:
+    """parses optional dependencies from the dictionary parse of a pyproject.toml file.
+    Returns a dictionary in the same format but with recursive references interpretted.
+    i.e., project[test] => pytest, pytest-cov"""
     extra_requirements = pyproject["project"]["optional-dependencies"]
     recursive_re = re.compile(project_name + r"\[.*\]")
     extra_re = re.compile(r"(?<=\[).*(?=\])")
 
-    def parse_extra(reqs):
+    def parse_extra(reqs: list[str]) -> list[str]:
+        """parses list of requirements and interprets recursive (e.g., project[test]) requirments"""
         parsed_reqs = []
         for req in reqs:
             if recursive_re.fullmatch(req):
@@ -95,7 +69,22 @@ def parse_extras_require(pyproject):
         extra_requirements[extra] = parse_extra(reqs)
     return extra_requirements
 
-setup_kwargs = {}
+
+# VARS
+pyproject_path: str = "pyproject.toml"
+pyproject: dict = _parse_pyproject(pyproject_path)
+project_name: str = normalize(pyproject["project"]["name"])
+src: str = pyproject["tool"]["bem"]["source-directory"]
+dirs: list[str] = [".tmp", ".tmp/download", ".tmp/logs"]
+shelf_name: str = ".tmp/download/bash-environment-shelf.zip"
+
+
+# BEM setup and framework import
+create_dirs(dirs)
+install_codepacks(pyproject.get("tool", {}).get("bem", {}).get("codepack", {}))
+
+# setuptools setup
+setup_kwargs: dict = {}
 if not src:
     setup_kwargs["packages"] = [project_name]
 else:
@@ -110,8 +99,6 @@ setup(
     author=[auth for auth in pyproject["project"]["authors"]],
     author_email=[auth for auth in pyproject["project"]["authors"]],
     license_files=pyproject["project"]["license"],
-    # package_dir={"": src},
-    # packages=found_packages,
     zip_safe=False,
     include_package_data=True,
     python_requires=pyproject["project"]["requires-python"],
@@ -121,5 +108,5 @@ setup(
         [console_scripts]
     """
     + f"{project_name}={project_name}.__main__:main",
-    **setup_kwargs
+    **setup_kwargs,
 )
